3fe5728b90076e36bd1ec308d9734ca848b6248f
empoluboyarov <poluboyarovem@gmail.com>
2016-04-06 22:26:58 +0300

    Version 0.2

--- app/src/main/java/com/example/evgeniy/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/example/evgeniy/starbuzz/TopLevelActivity.java
@@ -1,7 +1,11 @@
 package com.example.evgeniy.starbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0){
+                    Intent intent = new Intent(this,DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        ListView listView = (ListView)findViewById(R.id.listView);
+        listView.setOnItemClickListener(listener);
     }
 }
